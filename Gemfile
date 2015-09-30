6a382b5ef191f34a2c90180f0093b8aae6ff0035
Maikel Traksel <maikel_t14@hotmail.com>
2015-09-30 12:57:49 +0200

    deploy settings

--- Gemfile
+++ Gemfile
@@ -43,5 +43,11 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+group :development, :test do
+  gem 'capistrano', '~> 3.2'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+end
 end
 
---
+++ config/deploy/develop.rb
@@ -0,0 +1,14 @@
+set :stage, :develop
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server '188.166.25.244:2508', user: 'deploy', roles: %w{web app db}
+
+set :deploy_to, "/home/deploy/railsapps/seriousgamemaster/develop"
+
+set :rails_env, 'development'
+set :branch, 'develop'
