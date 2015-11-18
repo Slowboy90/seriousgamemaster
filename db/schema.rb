bc8ee6135cb2a65fb4e4b1b1fc8343da02ac9647
YOUR NAME <YOUR@EMAIL.com>
2015-11-18 14:29:15 +0100

    Created model_feedback and described the relation with model_user and model_learning objective

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118125920) do
+ActiveRecord::Schema.define(version: 20151118131835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "learning_objective_id"
+    t.integer  "user_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "proofs", force: :cascade do |t|
     t.text     "description"
