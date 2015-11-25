d96a1273d6aae3796c11e14f69a89a0bbab810db
Maikel Traksel <maikel_t14@hotmail.com>
2015-11-25 12:22:18 +0100

    Added User realtions in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # relations
+  has_many :feedbacks
+  has_many :student_feedbacks, class_name: Feedback, foreign_key: :student_id
+  has_many :learning_objectives
 end
