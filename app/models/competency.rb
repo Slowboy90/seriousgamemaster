be9a32a4c6d85303b6e3d3184577b697637ebac7
Maikel Traksel <maikel_t14@hotmail.com>
2015-11-25 10:15:16 +0100

    changed relations of models and tiny model fixes

--- app/models/competency.rb
+++ app/models/competency.rb
@@ -3,4 +3,6 @@ class Competency < ActiveRecord::Base
   has_many :learning_objectives
   has_many :feedbacks, through: :competency_feedbacks
   has_many :competency_feedbacks
+
+  accepts_nested_attributes_for :feedbacks, :competency_feedbacks
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,10 @@
 # Model class Feedback
 class Feedback < ActiveRecord::Base
   belongs_to :user
+  belongs_to :student, class_name: User, foreign_key: :student_id
   belongs_to :learning_objective
-  has_many :competencies, through: :competency_feedback
+  has_many :competencies, through: :competency_feedbacks
+  has_many :competency_feedbacks
+
+  accepts_nested_attributes_for :competencies, :competency_feedbacks
 end
