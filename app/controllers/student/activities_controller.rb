7e67ae31a6b2c25b35030cce862c0f4f2412dcd2
Maikel Traksel <maikel_t14@hotmail.com>
2015-12-10 11:01:53 +0100

    Deleted the form partial, made the form in edit and new views, fixed all routing and added destroy function

--- app/controllers/student/activities_controller.rb
+++ app/controllers/student/activities_controller.rb
@@ -8,20 +8,45 @@ class Student::ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
   end
 
+  def new
+    @activity = Activity.new
+  end
+  
   def create
-    @activity = Activity.new(activity_params)
+    @activity = LearningObjective.find(@learning_objective).activities.new(activity_params)
 
     if @activity.save
-      redirect_to admin_root_path, notice: 'Competentie succesvol aangemaakt'
+      redirect_to student_learning_objective_activities_path(@learning_objective), notice: 'Activiteit succesvol aangemaakt'
     else
       render "new"
     end
   end
 
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+
+  def update
+    @activity = Activity.find(params[:id])
+
+    if @activity.update(activity_params)
+      redirect_to student_learning_objective_activities_path(@learning_objective), notice: 'Activiteit succesvol aangepast'
+    else
+      render "edit"
+    end
+  end
+
+    def destroy
+    @activity = Activity.find(params[:id])
+    @activity.destroy
+    redirect_to student_learning_objective_activities_path(@learning_objective), notice: "Activiteit succesvol verwijderd"
+  end
+
+
   private
 
   def activity_params
     params.require(:activity)
-      .permit(:description)
+      .permit(:description, :learning_objective_id, :context_id)
   end
 end
