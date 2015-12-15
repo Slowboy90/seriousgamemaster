81514c8bb8e49d8fff7aceccb6aebf8dbb44453d
Maikel Traksel <maikel_t14@hotmail.com>
2015-12-15 13:45:45 +0100

    more fixes

--- app/controllers/student/competencies_controller.rb
+++ app/controllers/student/competencies_controller.rb
@@ -1,4 +1,6 @@
 class Student::CompetenciesController < ApplicationController
-  def show
-  end
+  # before_action { @competency = params[:competency_id] }
+  # def show
+  #   @learning_objectives = LearningObjective.where(user_id: current_user.id, competency_id: @competency)
+  # end
 end
