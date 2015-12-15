303964e4f6fa8639054f59462c3ded6def63229b
YOUR NAME <YOUR@EMAIL.com>
2015-12-15 11:28:43 +0100

    Changed seed, and admin dashboard

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -3,7 +3,7 @@ class Admin::UserController < ApplicationController
   def index
     @student = User.where('usertype = 1')
 	  @teacher = User.where('usertype = 2')
-    @student = User.where('usertype = 3')
+    @admin = User.where('usertype = 3')
 
     # @all_students = Student.all.count
   end
