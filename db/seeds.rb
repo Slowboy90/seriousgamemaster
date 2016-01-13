f26c0a3d87e6d02715146c445528e7955dd1ec57
Maikel Traksel <maikel_t14@hotmail.com>
2016-01-13 09:20:29 +0100

    queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Studenten
+# Students
 student1 = User.create(email: 'kraa1400@nhl.nl', password: '1234567890', usertype: 1, education_name: 'Bruno vd Kraan', education_number: 1)
 student2 = User.create(email: 'slot1400@nhl.nl', password: '1234567890', usertype: 1, education_name: 'Coen Slot', education_number: 1)
 student3 = User.create(email: 'lepp1400@nhl.nl', password: '1234567890', usertype: 1, education_name: 'Jorik Leppink', education_number: 1)
 student4 = User.create(email: 'trak1400@nhl.nl', password: '1234567890', usertype: 1, education_name: 'Maikel Traksel', education_number: 1)
 student5 = User.create(email: 'huis1400@nhl.nl', password: '1234567890', usertype: 1, education_name: 'Marrick Huisma', education_number: 1)
 
-# Docenten
+# Teachers
 teacher1 = User.create(email: 'docent1@nhl.nl', password: '1234567890', usertype: 2, education_name: 'Docent 1', education_number: 2)
 teacher2 = User.create(email: 'docent2@nhl.nl', password: '1234567890', usertype: 2, education_name: 'Docent 2', education_number: 2)
 teacher3 = User.create(email: 'docent3@nhl.nl', password: '1234567890', usertype: 2, education_name: 'Docent 3', education_number: 2)
@@ -49,3 +49,13 @@ context3 = Context.create(description: 'Context 1 bij bij activiteit 3', activit
 proof1 = Proof.create(description: 'Bewijs 1 bij activiteit 1', activity_id: 1)
 proof2 = Proof.create(description: 'Bewijs 2 bij activiteit 1', activity_id: 1)
 proof3 = Proof.create(description: 'Bewijs 3 bij activiteit 1', activity_id: 1)
+
+# Create feedback where the teacher is teacher1 (user with id 6)
+Feedback.create(learning_objective_id: 1, user_id: 6, student_id: 1)
+
+# Making and saving the relation between feedback and competency
+Feedback.first.competencies.build.save
+
+# setting test equal to the relation
+test = Feedback.first.competencies.build
+test = Feedback.first.competencies << test
